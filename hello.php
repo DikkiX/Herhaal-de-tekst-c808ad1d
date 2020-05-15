2acfd02a186511fe937a257cec573e990a3cb4ec
Obed_Adomako <2079361@talnet.nl>
2020-05-15 20:20:28 +0200

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,15 @@
+<?PHP
+
+$Naam = "obed";
+$Achternaam = "fritz";
+$Leeftijdeeftijd = "19";
+$Oogkleur = "bruin";
+
+
+echo "mijn naam is $Naam "."<br />";
+echo "mijn naam is $Naam $Achternaam"."<br />";
+echo "mijn naam is $Naam $Achternaam en ik ben $Leeftijdeeftijd jaar oud"."<br />";
+echo "mijn naam is $Naam $Achternaam en ik ben $Leeftijdeeftijd jaar oud  en mijn oogkleur is $Oogkleur "."<br />";
+
+
+?>
